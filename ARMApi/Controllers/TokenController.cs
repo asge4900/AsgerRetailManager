ad8d47c3ba8af22f498603222b427133b23bc195
asge4900 <48907330+asge4900@users.noreply.github.com>
2020-08-23 17:50:21 +0200

    Video: 35 Time: 24:00

---
+++ ARMApi/Controllers/TokenController.cs
@@ -0,0 +1,25 @@
+﻿using ARMApi.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ARMApi.Controllers
+{
+    public class TokenController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        private readonly UserManager<IdentityUser> userManager;
+
+        public TokenController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            this.context = context;
+            this.userManager = userManager;
+        }
+
+        private async Task<bool> IsValidUsernameAndPassword(string username, string password)
+        {
+            var user = await userManager.FindByEmailAsync(username);
+            return await userManager.CheckPasswordAsync(user, password);
+        }
+    }
+}
